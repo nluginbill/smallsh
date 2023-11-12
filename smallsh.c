71dc32be7a60da32121d975f1f7eefeed48f0f2d
nluginbill <nluginbill@gmail.com>
2023-11-12 12:39:27 -0500

    test 14c

--- smallsh.c
+++ smallsh.c
@@ -16,6 +16,7 @@
 
 char *words[MAX_WORDS];
 pid_t last_background_pid = -5;
+pid_t last_background_exit_status = 0;
 pid_t last_foreground_exit_status = 0;
 pid_t smallsh_pid = -5;
 size_t wordsplit(char const *line);
@@ -258,10 +259,25 @@ int main(int argc, char *argv[])
           }
           default:
           // parent process
+          int status;
+          if (background == 1) {
+            last_background_pid = waitpid(pid, &status, WNOHANG );
+
+            if (WIFSIGNALED(status)) {
+              last_background_exit_status = WTERMSIG(status) + 128;
+            } else {
+              last_background_exit_status = WEXITSTATUS(status);
+              printf("Child process %d done. Exit status %d.\n", pid, last_background_exit_status);
+            }
+          if (last_background_pid == -1) {
+            fprintf(stderr, "waitpid: %s\n", strerror(errno));
+          }
+          break;
+          }
           // if the command is not a background process (foreground process)
           if (background == 0) {
             // wait for the child process to finish
-            int status;
+            
             pid_t last_foreground_pid = waitpid(pid, &status, 0);
             if (last_foreground_pid == -1) {
               fprintf(stderr, "waitpid: %s\n", strerror(errno));
@@ -271,8 +287,10 @@ int main(int argc, char *argv[])
             } else {
               last_foreground_exit_status = WEXITSTATUS(status);
             }
-          }
           break;
+            
+          }
+
         }
         
       }
